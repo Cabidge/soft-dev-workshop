437e99e73eca6d180fcceb9a0668109417190510
Cabidge <cabbageboi69@gmail.com>
2021-09-22 20:29:49 -0400

    Added comments and header

--- cw0.py
+++ cw0.py
@@ -1,3 +1,10 @@
+"""
+Wen Hao Dong
+SoftDev
+Create two lists of names, one for period one and one for period two, and print a random name.
+2021-09-20
+"""
+
 import random
 
 pd1 = [
@@ -14,6 +21,7 @@ pd2 = [
     "Doe Doe",
 ]
 
+# Chooses a random period then a random name from said period
 rand_list = random.choice([pd1, pd2])
 rand_name = random.choice(rand_list)
 print(rand_name)
