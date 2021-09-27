57d3b5d52e89626f44eb9d83f12e2402d94116d8
Cabidge <cabbageboi69@gmail.com>
2021-09-27 09:17:04 -0400

    Upadated lists to be a single dictionary

--- 05_py/main.py
+++ 05_py/main.py
@@ -1,32 +1,31 @@
 """
 Wen Hao Dong
 SoftDev
-Random Name Selector Amalgamate
+K05 -- Random Name Selector Amalgamate
 2021-09-20
 """
 
-# Will be updated once partners respond to my email
-
 import random
 
-pd1 = [
-    "Owen Yaggy", "Haotian Gan", "Ishraq Mahid", "Ivan Lam", "Michelle Lo"
-    "Christopher Liu", "Ivan Mijacika", "Lucas Lee", "Josephine Lee", "Rayat Roy",
-    "Emma Buller", "William Chen", "Rachel Xiao", "Andrew Juang", "Yuqing Wu"
-    "Renggeng Zheng", "Annabel Zhang", "Alejandro Alonso", "Deven Maheshwari",
-    "Oscar Wang", "Tami Takada", "Yusuf Elsharawy", "Ella Krechmer", "Tomas Acuna",
-    "Tina Nguyen", "Theo Fahey", "Sadid Ethun"
-]
-
-pd2 = [
-    "Patrick Ging", "Wenhao Dong", "Raymond Yeung", "Kevin Cao", "Michael Borczuk",
-    "Alif Abdullah", "Justin Zou", "Andy Lin", "Shadman Rakib", "David Chong",
-    "Daniel Sooknanan", "Cameron Nelson", "Austin Ngan", "Yaying Liang Li", "Eric Guo",
-    "Noakai Aronesty", "Roshani Shrestha", "Yoonah Chang",
-    "Qina Liu", "Han Zhang", "Joshua Kloepfer"
-]
+classes = {
+    "pd1": [
+        "Owen Yaggy", "Haotian Gan", "Ishraq Mahid", "Ivan Lam", "Michelle Lo"
+        "Christopher Liu", "Ivan Mijacika", "Lucas Lee", "Josephine Lee", "Rayat Roy",
+        "Emma Buller", "William Chen", "Rachel Xiao", "Andrew Juang", "Yuqing Wu"
+        "Renggeng Zheng", "Annabel Zhang", "Alejandro Alonso", "Deven Maheshwari",
+        "Oscar Wang", "Tami Takada", "Yusuf Elsharawy", "Ella Krechmer", "Tomas Acuna",
+        "Tina Nguyen", "Theo Fahey", "Sadid Ethun"
+    ],
+    "pd2": [
+        "Patrick Ging", "Wenhao Dong", "Raymond Yeung", "Kevin Cao", "Michael Borczuk",
+        "Alif Abdullah", "Justin Zou", "Andy Lin", "Shadman Rakib", "David Chong",
+        "Daniel Sooknanan", "Cameron Nelson", "Austin Ngan", "Yaying Liang Li", "Eric Guo",
+        "Noakai Aronesty", "Roshani Shrestha", "Yoonah Chang",
+        "Qina Liu", "Han Zhang", "Joshua Kloepfer"
+    ]
+}
 
 # Chooses a random period then a random name from said period
-rand_list = random.choice([pd1, pd2])
+rand_list = random.choice(list(classes.keys()))
 rand_name = random.choice(rand_list)
 print(rand_name)
