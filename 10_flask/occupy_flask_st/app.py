18c667ec2f3fd404376878b9825fa03bf1b2ca1d
Cabidge <cabbageboi69@gmail.com>
2021-10-05 18:41:46 -0400

    Updated heading

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -1,3 +1,7 @@
+# Fabulous Frogs: David Chong (with Fluff), Wen Hao Dong (with Jal Hordan), and Austin Ngan (with Gerald)
+# SoftDev
+# 2021-10-04
+
 from flask import Flask
 import occupations
 app = Flask(__name__)
@@ -11,7 +15,7 @@ def h():
             Wen Hao Dong (with Jal Hordan),
             and Austin Ngan (with Gerald)
         </h1>
-        {"<br>".join(occupations.jobs)} 
+        {"<br>".join(occupations.jobs)}
         <br>
         <b>Random weighted occupation:</b> {occupations.weighted_selection()}
     """
