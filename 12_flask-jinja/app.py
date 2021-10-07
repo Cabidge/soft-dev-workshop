5cf9af919f04b600de8d2993d3967abd31cf34ae
Cabidge <cabbageboi69@gmail.com>
2021-10-07 18:55:13 -0400

    Updated heading

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -1,6 +1,7 @@
-# Clyde 'Thluffy' Sinclair
+# The Fabulous Frogs (David Chong, Wen Hao Dong, Austin Ngan)
 # SoftDev
-# Oct 2021
+# K12 -- Flask Templates
+# 2021-10-07
 
 from flask import Flask, render_template #Q0: What happens if you remove render_template from this line?
 app = Flask(__name__)
