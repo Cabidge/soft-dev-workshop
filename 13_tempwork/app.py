86c38970e797166c90e6d59f88e6fdd7719fb406
Cabidge <cabbageboi69@gmail.com>
2021-10-08 09:25:57 -0400

    Added random occupation to template

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import occupations
 
 app = Flask(__name__)
 
 @app.route("/occupyflaskst")
 def main():
-    return render_template("tablified.html")
+    return render_template("tablified.html", occ=occupations.random_occupation())
 
 if __name__ == "__main__":
     app.debug = True
