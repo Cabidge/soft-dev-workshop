e67f693c478c26dff17a2a605d3571164aac5d47
Cabidge <cabbageboi69@gmail.com>
2021-10-09 10:07:40 -0400

    Updated app.py to provide jobs to template

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -1,3 +1,8 @@
+#Team Cheese: Jesse Xie (with Polly), Wen Hao Dong (with Jal Hordan), Justin Zou (with Piggy)
+#SoftDev
+#K13 -- Template for Success
+#2021-10-08
+
 from flask import Flask, render_template
 import occupations
 
@@ -5,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/occupyflaskst")
 def main():
-    return render_template("tablified.html", occ=occupations.random_occupation())
+    return render_template("tablified.html", occ=occupations.random_occupation(),joblist = occupations.jobs)
 
 if __name__ == "__main__":
     app.debug = True
