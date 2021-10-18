9486f04239289f04c6f050e7e3f44a15643124f2
Cabidge <cabbageboi69@gmail.com>
2021-10-18 13:40:32 -0400

    Added heading

--- 15_session/app.py
+++ 15_session/app.py
@@ -1,3 +1,8 @@
+# Selective Soup | Justin Zou (Piggy), Wen Hao Dong (Jal Hordan), Roshani Shrestha (Pete)
+# SoftDev
+# K15: Sessions Greetings
+# 2021-10-18
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
