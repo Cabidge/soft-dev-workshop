b499c48a0825ba86d63fb1b2d33d1c553a780a94
Cabidge <cabbageboi69@gmail.com>
2021-10-20 21:21:45 -0400

    Merged from teammate

--- 15_session/app.py
+++ 15_session/app.py
@@ -1,6 +1,6 @@
 # Selective Soup | Justin Zou (Piggy), Wen Hao Dong (Jal Hordan), Roshani Shrestha (Pete)
-# SoftDev
-# K15: Sessions Greetings - Uses session capabilities to allow a user to login and logout
+# SoftDev 
+# K15: Sessions Greetings - Uses session capabilities to allow a user to login and logout 
 # 2021-10-18
 
 from sys import argv
@@ -13,8 +13,8 @@ app = Flask(__name__)
 @app.route("/", methods = ['POST', 'GET']) #, methods=['GET', 'POST'])
 def disp_loginpage(): # displays the initial login page or welcome page if the user is logged in
     """
-    Sessions are used to see if the user is logged in.
-    If so, it displays the welcome page at the root, which is created by rendering response.html.
+    Sessions are used to see if the user is logged in. 
+    If so, it displays the welcome page at the root, which is created by rendering response.html. 
     If not, it displays the login page at the root, which is created by rendering login.html.
     The username and password are checked to see if they are correct, which are compared to a hardcoded single username/password combination.
     Messages are displayed if login fails.
@@ -24,13 +24,13 @@ def disp_loginpage(): # displays the initial login page or welcome page if the u
             if (request.form['username'] == 'Selective Soup' and request.form['password'] == 'hello'): # checks if the username and password are both correct
                 session['user'] = request.form['username'] # adds session data
             else: # the case that the username and password are not both correct
-                # print("***DIAG: request.form ***")
+                # print("***DIAG: request.form ***") 
                 # print(request.form)
-                # print("***DIAG: request.form['username'] ***")
+                # print("***DIAG: request.form['username'] ***") 
                 # print(request.form['username'])
-                # print("***DIAG: request.form['password'] ***")
+                # print("***DIAG: request.form['password'] ***") 
                 # print(request.form['password'])
-                if (request.form['username'] != 'Selective Soup' and request.form['password'] != 'hello'): # checks if both are incorrect
+                if (request.form['username'] != 'Selective Soup' and request.form['password'] != 'hello'): # checks if both are incorrect 
                     return render_template( 'login.html', logged = "Wrong username and password")
                 elif (request.form['username'] != 'Selective Soup'): # checks if only username is incorrect
                     return render_template( 'login.html', logged = "Wrong username")
@@ -40,7 +40,7 @@ def disp_loginpage(): # displays the initial login page or welcome page if the u
             print('request.form does not exist')
 
     if "user" in session: # checks if the user is logged in
-        # print("***DIAG: session['user'] ***")
+        # print("***DIAG: session['user'] ***") 
         # print(session['user'])
         return render_template('response.html', username = session['user'])
     else:
@@ -50,7 +50,7 @@ def disp_loginpage(): # displays the initial login page or welcome page if the u
 def logoutPg(): # returns to the login page after the user presses the Logout button
     """
     Redirects to the login page after the user presses the Logout button, allowing the user to log back in.
-    Removes the session.
+    Removes the session.  
     """
     session.pop('user') # removes the session
     return redirect('http://localhost:5000/') # redirects the page back to the login page, which is at the root
