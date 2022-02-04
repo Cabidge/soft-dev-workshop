ed676d827fc2b68ff9930ed26a6463f7628f0b50
Cabidge <cabbageboi69@gmail.com>
2022-02-04 08:18:48 -0500

    Set up 27

---
+++ 27_js/fibfac.scm
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+; Define a function of n, fact, such that if n is equal to 0, it returns 1, otherwise, it returns the product of fact of n - 1 and n
+(define fact
+  (lambda (n)
+    (if (= n 0) 1 (* (fact (- n 1)) n))))
+
+"Fact"
+(fact 1)
+(fact 2)
+(fact 3)
+(fact 4)
+(fact 5)
+(fact 6)
+
+(define fib
+  (lambda (n)
+    (if (< n 2)
+        n
+        (+ (fib (- n 1)) (fib (- n 2))))))
+"Fib"
+(fib 0)
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
+(fib 6)
