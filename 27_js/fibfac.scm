79c979c9ce49db93ab6e35345b9dc0d780df2382
Cabidge <cabbageboi69@gmail.com>
2022-02-04 08:34:08 -0500

    Header

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -1,6 +1,10 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname rkt) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+; Wen Hao Dong, Haotian Gan
+; SoftDev
+; K27 -- Where Does JS Live?
+; 2022-02-07
 ; Define a function of n, fact, such that if n is equal to 0, it returns 1, otherwise, it returns the product of fact of n - 1 and n
 (define fact
   (lambda (n)
